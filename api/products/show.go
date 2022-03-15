01b842f30b30ce7d53841363b70ff152f061476a
ruiborda <bordarui@gmail.com>
2022-03-15 12:01:22 -0500

    GET /products/:id

--- api/products/show.go
+++ api/products/show.go
@@ -1,7 +1,27 @@
 package products
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/ruiborda/go-mongodb-crud-api-rest/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func ShowProducts(c *fiber.Ctx) error {
-	return c.Status(200).SendString("Product " + c.Params("id"))
+	_id, err := primitive.ObjectIDFromHex(c.Params("id"))
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	m := database.Mongo{}
+	m.Open()
+	defer m.Close()
+
+	var result Model
+
+	if err := m.Database.Collection("products").FindOne(c.Context(), bson.M{"_id": _id}).Decode(&result); err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(result)
 }
